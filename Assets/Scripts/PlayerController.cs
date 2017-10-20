27016d91591e53d19e6e31e48b2d9b74168cda97
ChrisHammie <Christopher2.Hamilton@live.uwe.ac.uk>
2017-10-20 14:42:55 +0100

    Setup for PLayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float maxVelocity = 5.0f;
+	public float jumpSpeed = 5.0f;
+
+
+	public enum inputState
+	{
+		MouseKeyboard,
+		Xbox360,
+		PS3
+	}
+
+
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+
+	}
+}
