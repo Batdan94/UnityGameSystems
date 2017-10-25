ccfa594bc4c1201901e9e0cbbe2ffbf29e6ffbb5
ChrisHammie <christopher2.hamilton@live.uwe.ac.uk>
2017-10-25 22:38:08 +0100

    Stupidly basic start with fist coming down from sky

---
+++ Assets/circleMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleMouse : MonoBehaviour
+{
+
+
+
+
+
+
+
+    void Update()
+    {
+
+        Vector3 mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0.0f - Camera.main.transform.position.z);
+        mouse = Camera.main.ScreenToWorldPoint(mouse);
+
+        this.transform.position = new Vector3(mouse.x, mouse.y, 0.0f);
+    }
+}
